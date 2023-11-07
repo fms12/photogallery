e3d6c80b095eca85c42f9b3da958afcff8dcb1f9
fms12 <mayank2001.iimt@gmail.com>
2023-11-07 13:43:06 +0530

    src folder and other modules

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav,Body } from './layout/index'
+function Home() {
+  return (
+    <React.Fragment>
+        <Nav/>
+        <Body />
+    </React.Fragment>
+  )
+}
+
+export default Home
---
+++ src/components/layout/Body.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Body() {
+  return (
+    <div>
+
+    </div>
+  
+  );
+}
+
